53212abd0734ce337a6e9d649922a1661cd604f1
sherin2022 <sherinj@maveric-systems.com>
2022-03-22 12:29:36 +0530

    adding post api

---
+++ src/main/java/com/example/demo/model/PostResponse.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostResponse {
+
+    private String postId;
+    private String post;
+    private String postedBy; //User Id who posted.
+    private Integer likesCount;
+    private Integer commentsCount;
+    private Date createdAt;
+    private Date updatedAt;
+
+}
---
+++ src/main/java/com/example/demo/service/PostServiceImpl.java
@@ -0,0 +1,59 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Post;
+import com.example.demo.model.PostResponse;
+import com.example.demo.repo.PostRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    PostRepo postRepo;
+
+
+    @Override
+    public List<Post> getPosts() {
+        return postRepo.findAll();
+    }
+
+    @Override
+    public PostResponse createPost(Post post) {
+
+        post.setCreatedAt(new Date());
+        post.setUpdatedAt(new Date());
+
+        PostResponse postResponse= new PostResponse();
+
+        postResponse.setPostId(post.getPostId());
+        postResponse.setPost(post.getPost());
+        postResponse.setPostedBy(post.getPostedBy());
+        postResponse.setCommentsCount(0);
+        postResponse.setLikesCount(0);
+        postResponse.setCreatedAt(post.getCreatedAt());
+        postResponse.setUpdatedAt(post.getUpdatedAt());
+
+        postRepo.save(new Post(post.getPostId(),post.getPost(),post.getPostedBy(),post.getCreatedAt(),post.getUpdatedAt()));
+
+        return postResponse;
+    }
+
+    @Override
+    public Post getPostDetails(String id) {
+        return postRepo.findByPostId(id);
+    }
+
+    @Override
+    public Post updatePost(String id, Post post) {
+        return postRepo.save(new Post(post.getPostId(),post.getPost(),post.getPostedBy(),post.getCreatedAt(),post.getUpdatedAt()));
+    }
+
+    @Override
+    public Post deletePost(String id) {
+        return postRepo.deleteByPostId(id);
+    }
+}
